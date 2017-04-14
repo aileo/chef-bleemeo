a94c752106e265e1463573555c4dbe9191bc8a76
lbonnargent <lbonnargent@matlo.com>
2017-04-14 10:22:29 +0200

    Release v0.1.0 to chef supermarket

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,16 @@
 name             'bleemeo'
-maintainer       'aileo'
+maintainer       'matlo'
 maintainer_email 'lbonnargent@matlo.com'
 license          'All rights reserved'
 description      'Installs/Configures bleemeo-agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+issues_url 'https://github.com/aileo/chef-bleemeo/issues'
+source_url 'https://github.com/aileo/chef-bleemeo'
+license 'MIT'
+
+supports 'ubuntu', '= 16.40'
+supports 'debian', '= 8.7'
+supports 'centos', '= 7.3'
+supports 'fedora', '= 24.0'
